b8aaaf76b15514f15bb1ec389e49aa8ecb233ec0
Howie <howie12207@gmail.com>
2021-10-16 23:16:09 +0800

    'deploy'

--- assets/Contact.e34da20c.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.e6d06783.js";import{o as e,b as o}from"./vendor.7c19785e.js";var n=r({},[["render",function(r,n){return e(),o("div",null,"contact")}]]);export{n as default};
